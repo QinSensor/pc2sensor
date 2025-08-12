0d836ca91335b6f7d0c8a1f2c5d216ca4af58a88
qinji <qinjiang19871018@gmail.com>
2025-08-12 16:04:36 +0100

    begin get data77

--- ble_connect.py
+++ ble_connect.py
@@ -1,14 +1,24 @@
 
-def connect_sensor(self):
-    # Delegate to parent
-    self.parent.connect_device(self.address)
-    self.conn_status.set("Connected")
-    self.update_button_states()
+# def connect_sensor(self):
+#     # Delegate to parent
+#     self.parent.connect_device(self.address)
+#     self.conn_status.set("Connected")
+#     self.update_button_states()
+#
+#
+# def disconnect_sensor(self):
+#     # Delegate to parent
+#
+#     self.parent.disconnect_device(self.address)
+#     self.conn_status.set("Disconnected")
+#     self.update_button_states()
 
+def connect_sensor(app):
+    app.parent.connect_device(app.address)
+    app.conn_status.set("Connected")
+    app.update_button_states()
 
-def disconnect_sensor(self):
-    # Delegate to parent
-
-    self.parent.disconnect_device(self.address)
-    self.conn_status.set("Disconnected")
-    self.update_button_states()
+def disconnect_sensor(app):
+    app.parent.disconnect_device(app.address)
+    app.conn_status.set("Disconnected")
+    app.update_button_states()
